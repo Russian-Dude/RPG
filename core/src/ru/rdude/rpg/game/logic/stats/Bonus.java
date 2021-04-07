f3695c2c90e558bf3a4d21757d2fb7194034601f
TheRussianDude <hritnik@yandex.ru>
2021-04-07 23:48:23 +0300

    Bonus allows to set custom stat values which however can be changed with leveling

---
+++ core/src/ru/rdude/rpg/game/logic/stats/Bonus.java
@@ -0,0 +1,14 @@
+package ru.rdude.rpg.game.logic.stats;
+
+public class Bonus {
+
+    private double value = 0d;
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
