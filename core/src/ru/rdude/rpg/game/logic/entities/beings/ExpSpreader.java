52255ba230c86c09b5692b69c3cc8005fa88a310
TheRussianDude <hritnik@yandex.ru>
2021-04-08 03:11:56 +0300

    Now observe game state changes so it can automatically refresh itself

--- core/src/ru/rdude/rpg/game/logic/entities/beings/ExpSpreader.java
+++ core/src/ru/rdude/rpg/game/logic/entities/beings/ExpSpreader.java
@@ -2,11 +2,12 @@ package ru.rdude.rpg.game.logic.entities.beings;
 
 import ru.rdude.rpg.game.logic.coefficients.Coefficients;
 import ru.rdude.rpg.game.logic.entities.skills.Buff;
+import ru.rdude.rpg.game.logic.game.Game;
+import ru.rdude.rpg.game.logic.gameStates.GameStateBase;
+import ru.rdude.rpg.game.logic.gameStates.GameStateObserver;
 import ru.rdude.rpg.game.logic.stats.Stat;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,7 +16,7 @@ import java.util.stream.Stream;
  * Calculates how useful each player was in the battle
  */
 
-public class ExpSpreader implements BeingActionObserver {
+public class ExpSpreader implements BeingActionObserver, GameStateObserver {
 
     private Party party;
     private Map<Being, Effort> effort = new HashMap<>();
@@ -26,6 +27,7 @@ public class ExpSpreader implements BeingActionObserver {
             being.subscribe(this);
             effort.put(being, new Effort(being));
         });
+        Game.getCurrentGame().getGameStateHolder().subscribe(this);
     }
 
     public Map<Being, Double> spreadExp(double exp) {
@@ -38,12 +40,30 @@ public class ExpSpreader implements BeingActionObserver {
         double partySize = party.getBeings().size();
         double totalEffort = effort.values().stream().map(Effort::totalEffort).reduce(Double::sum).orElse(1d);
         double beingEffort = effort.get(being).totalEffort();
-        return (1 + intBonus) * (allExp * 0.07 + allExp * (1 - partySize * 0.07) * (100 / ( totalEffort / beingEffort)));
+        return (1 + intBonus) * (allExp * 0.07 + allExp * (1 - partySize * 0.07) * (100 / (totalEffort / beingEffort)));
+    }
+
+    public void clear() {
+        // update effort map in case party was was changed
+        effort.keySet().stream()
+                .filter(being -> !party.getBeings().contains(being))
+                .collect(Collectors.toList())
+                .forEach(effort::remove);
+        party.getBeings().stream()
+                .filter(being -> !effort.containsKey(being))
+                .collect(Collectors.toList())
+                .forEach(being -> effort.put(being, new Effort(being)));
+        effort.values().forEach(Effort::clear);
     }
 
     @Override
     public void update(BeingAction action, Being being) {
         effort.get(being).madeAction(action);
+    }
+
+    @Override
+    public void update(GameStateBase oldValue, GameStateBase newValue) {
+        clear();
     }
 
     private class Effort {
@@ -55,10 +75,16 @@ public class ExpSpreader implements BeingActionObserver {
 
         private double value = 0d;
 
-        public Effort(Being being) {
+        Effort(Being being) {
             this.being = being;
         }
 
+        void clear() {
+            hitsDeal = 0;
+            hitsReceived = 0;
+            buffsDeal = 0;
+            value = 0;
+        }
 
         double totalEffort() {
             return value + hitsDeal * 5 + hitsReceived * 3 + buffsDeal * 6;
@@ -69,11 +95,9 @@ public class ExpSpreader implements BeingActionObserver {
             Buff buffInteractor = null;
             if (action.interactor() instanceof Being) {
                 beingInteractor = (Being) action.interactor();
-            }
-            else if (action.interactor() instanceof Buff) {
+            } else if (action.interactor() instanceof Buff) {
                 buffInteractor = (Buff) action.interactor();
-            }
-            else {
+            } else {
                 return;
             }
             switch (action.action()) {
@@ -111,8 +135,7 @@ public class ExpSpreader implements BeingActionObserver {
                     if (party.getBeings().contains(beingInteractor)) {
                         if (action.interactor().equals(being)) {
                             value += action.value() * 0.2;
-                        }
-                        else {
+                        } else {
                             value += action.value() * 0.7;
                         }
                         hitsDeal += 1;
@@ -124,13 +147,11 @@ public class ExpSpreader implements BeingActionObserver {
                     if (v > 0 && party.getBeings().contains(buffInteractor.getCaster())) {
                         if (being.equals(buffInteractor.getCaster())) {
                             value += v * 0.5;
-                        }
-                        else {
+                        } else {
                             value += v * 0.8;
                         }
                         buffsDeal += 1;
-                    }
-                    else if (v < 0) {
+                    } else if (v < 0) {
                         value += v * 0.7;
                         buffsDeal += 1;
                     }
