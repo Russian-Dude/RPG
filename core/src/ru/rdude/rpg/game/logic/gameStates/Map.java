723cd8c25def53a4f32f42d2002453b255d9fe28
Russian-Dude <ghritnik@gmail.com>
2021-10-21 17:04:40 +0300

    forEachCellProperties now iterates from the right bottom so sprites created inside this loop overlap each other correctly

--- core/src/ru/rdude/rpg/game/logic/gameStates/Map.java
+++ core/src/ru/rdude/rpg/game/logic/gameStates/Map.java
@@ -164,8 +164,8 @@ public class Map extends GameStateBase {
     }
 
     public void forEachCellProperties(BiConsumer<Cell, CellProperties> action) {
-        for (int x = 0; x < gameMap.getWidth(); x++) {
-            for (int y = 0; y < gameMap.getHeight(); y++) {
+        for (int x = gameMap.getWidth() - 1; x >= 0; x--) {
+            for (int y = gameMap.getHeight() - 1; y >= 0; y--) {
                 action.accept(gameMap.cell(x, y), cellProperties[x][y]);
             }
         }
